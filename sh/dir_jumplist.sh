ab460665b8eecf0c1833ea76a5970171e75a605e
Dugan Chen <dugan@node-1w7jr9qsft92wko50yahn3btz.ipv6.telus.net>
2026-05-01 10:49:40 -0700

    It's going to be one file with functions

---
+++ sh/dir_jumplist.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+_jgc() {
+	# Garbage collect the list
+
+	if [[ -f ~/.dir_jumplist.txt ]]; then
+		# https://stackoverflow.com/a/30988704
+		mapfile -t jumplist <~/.dir_jumplist.txt
+
+		for i in "${!jumplist[@]}"; do
+			if [[ ! -d "${jumplist[$i]}" ]]; then
+				unset "jumplist[$i]"
+			fi
+		done
+		jumplist=("${jumplist[@]}")
+
+		printf "%s\n" "${jumplist[@]}" >~/.dir_jumplist.txt
+	fi
+}
